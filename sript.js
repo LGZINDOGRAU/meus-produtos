8e1beefc03a3430d44336612f4900a39f8606425
LGZINDOGRAU <luis.kroessin@escola.pr.gov.br>
2025-11-03 09:35:35 -0300

    atualização

--- sript.js
+++ sript.js
@@ -1,14 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Rolagem suave para botão do hero
-  const botao = document.querySelector(".hero .btn");
-  botao.addEventListener("click", e => {
-    e.preventDefault();
-    const section = document.querySelector("#geleias");
-    section.scrollIntoView({ behavior: "smooth" });
-  });
-
-  // Rolagem suave para menu
-  document.querySelectorAll('header nav a[href^="#"]').forEach(link => {
+  document.querySelectorAll('header nav a, .hero .btn').forEach(link => {
     link.addEventListener("click", e => {
       e.preventDefault();
       const destino = document.querySelector(link.getAttribute("href"));
